0728d2639ec829741a70c22cc82a9e65c351d515
trxlezi <gabriel.caetano20@etec.sp.gov.br>
2022-05-18 11:56:57 -0300

    Começando a criação do BD

---
+++ database/migrations/2022_05_18_143800_create_measurements_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('measurements', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->doule('weight');
+            $table->double('height');
+            $table->double('chest');
+            $table->double('left_arm');
+            $table->double('right_arm');
+            $table->double('abdomen');
+            $table->double('Hips');
+            $table->double('left_thigh');
+            $table->double('right_thigh');
+            $table->double('left_calf');
+            $table->double('right_calf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('measurements');
+    }
+};
