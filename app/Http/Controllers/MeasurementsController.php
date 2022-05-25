36402aa8790aaffae2fa3255f248694e90891513
trxlezi <gabriel.caetano20@etec.sp.gov.br>
2022-05-25 14:36:14 -0300

    Quase terminado

--- app/Http/Controllers/MeasurementsController.php
+++ app/Http/Controllers/MeasurementsController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Measurements;
 use Illuminate\Http\Request;
 
 class MeasurementsController extends Controller
 {
-    //
+    public function store(Request $request){
+        //criando o objeto Medidas
+        $measurements = new Measurements();
+
+        // $measurements->date = $request->date;
+
+        //alterando os atributos do objeto
+
+        Measurements::create($request)
+        // $measurements = $request->all();
+
+        // $measurements->save();
+    }
 }
