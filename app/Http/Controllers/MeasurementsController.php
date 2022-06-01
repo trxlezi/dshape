5b45dca4a7fa9f5a37a3ad3db61910c7c5da5e1e
trxlezi <gabriel.caetano20@Etec.sp.gov.br>
2022-06-01 12:13:47 -0300

    Adicionado as medidas cadastradas

--- app/Http/Controllers/MeasurementsController.php
+++ app/Http/Controllers/MeasurementsController.php
@@ -11,13 +11,23 @@ class MeasurementsController extends Controller
         //criando o objeto Medidas
         $measurements = new Measurements();
 
-        // $measurements->date = $request->date;
-
         //alterando os atributos do objeto
+        $measurements->date = $request-> date;
+        $measurements->weight = $request -> weight;
+        $measurements->height = $request -> height;
+        $measurements->chest = $request -> chest;
+        $measurements->left_arm = $request -> left_arm;
+        $measurements->right_arm = $request-> right_arm;
+        $measurements->abdomen = $request-> abdomen;
+        $measurements->waist = $request -> waist;
+        $measurements->hips = $request -> hips;
+        $measurements->left_thigh = $request -> left_thigh;
+        $measurements->right_thigh = $request -> right_thigh;
+        $measurements->left_calf = $request -> left_calf;
+        $measurements->right_calf = $request -> right_calf;
 
-        Measurements::create($request)
-        // $measurements = $request->all();
 
-        // $measurements->save();
+        //Encaminha para salvar no bd
+        $measurements->save();
     }
 }
